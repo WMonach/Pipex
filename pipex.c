c722177e21de7b84a9d32063958dd35a439f7e59
william monachon <mimimonach@gmail.com>
2022-06-01 14:12:11 +0200

    get path, havce to fork

--- pipex.c
+++ pipex.c
@@ -1,19 +1,22 @@
 #include "pipex.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **envp)
 {
 	t_grp	*pipex;
 
 	pipex = malloc(sizeof(t_grp));
 	ft_get_init(pipex);
-	ft_parsing(pipex, argc, argv);
+	ft_parsing(pipex, argc, argv, envp);
 	return (0);
 }
 
-int	ft_parsing(t_grp *pipex, int argc, char **argv)
+int	ft_parsing(t_grp *pipex, int argc, char **argv, char **envp)
 {
 	ft_get_fd(pipex, argc, argv);
-	ft_get_cmdpath(pipex, argc, argv);
+	pipex->cmd1path = ft_get_cmdpath(argv[2], envp);
+	printf("%s\n", pipex->cmd1path);
+	pipex->cmd2path = ft_get_cmdpath(argv[3], envp);
+	printf("%s\n", pipex->cmd2path);
 	return (1);
 }
 
@@ -30,20 +33,81 @@ void	ft_get_fd(t_grp *pipex, int argc, char **argv)
 	close (pipex->infilefd);
 	pipex->outfilefd = open(argv[argc - 1], 0, O_RDONLY);
 	close (pipex->outfilefd);
-	// printf("%d\n", pipex->outfilefd);
-	// printf("%d\n", pipex->infilefd);
 }
 
-void	ft_get_cmdpath(t_grp *pipex, int argc, char **argv)
+char	*ft_get_cmdpath(char *argcmd, char **envp)
 {
-	pipex->pathlist = malloc(sizeof(t_grp) * ());
-	while (pipex)
+	char	**cmd;
+	char	*path;
+	char	**cmdpaths;
+	int		i;
+
+	i = 0;
+	cmd = ft_split(argcmd, ' ');
+	while (!ft_strnstr(envp[i], "PATH=", 5))
+		i++;
+	cmdpaths = ft_split(envp[i] + 5, ':');
+	i = -1;
+	while (cmdpaths[++i] != NULL)
 	{
+		path = ft_strjoin(cmdpaths[i], "/");
+		if (path == NULL)
+			return (ft_free_spliterr(cmd, cmdpaths));
+		path = ft_get_cmdpathbis(path, cmd, cmdpaths);
+		if (path != NULL)
+			return (path);
 	}
-	/*
-	trouver les differents PATH a tester et les rentrer dans pathlist en l'allouant
-	ensuite utiliser dans une boucle while les differents PATH avec access et partir
-	en n'oubliant pas de free pathlist
-	*/
+	// ft_free_split(cmd, cmdpaths);
+	return ("ha tu m'as eu batard");
 }
 
+char	*ft_get_cmdpathbis(char *path, char **cmd, char **cmdpaths)
+{
+	path = ft_strjoin(path, cmd[0]);
+	if (path == NULL)
+		return (ft_free_spliterr(cmd, cmdpaths));
+	if (access(path, X_OK) == 0)
+	{
+		ft_free_split(cmd, cmdpaths);
+		return (path);
+	}
+	return (NULL);
+}
+
+char	*ft_free_spliterr(char **to_free1, char **to_free2)
+{
+	int	i;
+
+	i = -1;
+	while (to_free1[++i] != NULL)
+		free(to_free1[i]);
+	free(to_free1);
+	i = -1;
+	while (to_free2[++i] != NULL)
+		free(to_free2[i]);
+	free(to_free2);
+	exit(0);
+	return (NULL);
+}
+
+void	ft_free_split(char **to_free1, char **to_free2)
+{
+	int	i;
+
+	i = -1;
+	while (to_free1[++i] != NULL)
+		free(to_free1[i]);
+	free(to_free1);
+	i = -1;
+	while (to_free2[++i] != NULL)
+		free(to_free2[i]);
+	free(to_free2);
+}
+
+/*
+preparer des fork pour faire des enfants
+dans ses enfants on dub les file 1 et 2
+pour changger leurs sorties et entrees
+quand tout est set on real les execve
+qui vont kill les 2 gosses
+*/
--- pipex.h
+++ pipex.h
@@ -6,18 +6,23 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <fcntl.h>
+# include "libft.h"
 
 typedef struct s_grp
 {
 	char	*cmd1path;
+	char	**cmdpaths;
 	char	*cmd2path;
 	int		infilefd;
 	int		outfilefd;
 	char	*pathlist;
 }	t_grp;
 
-int		ft_parsing(t_grp *pipex, int argc, char **argv);
+int		ft_parsing(t_grp *pipex, int argc, char **argv, char **envp);
 void	ft_get_init(t_grp *pipex);
 void	ft_get_fd(t_grp *pipex, int argc, char **argv);
-void	ft_get_cmdpath(t_grp *pipex, int argc, char **argv);
+char	*ft_get_cmdpath(char *argcmd, char **envp);
+char	*ft_get_cmdpathbis(char *path, char **cmd, char **cmdpaths);
+char	*ft_free_spliterr(char **to_free1, char **to_free2);
+void	ft_free_split(char **to_free1, char **to_free2);
 #endif
