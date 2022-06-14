490f942380eac688e89bf66692559f6e541ea716
WMonach <mimimonach@gmail.com>
2022-06-14 08:40:25 +0200

    change home

--- pipex.c
+++ pipex.c
@@ -4,6 +4,9 @@ int	main(int argc, char **argv)
 {
 	t_grp	pipex;
 
+	ft_get_init(&pipex);
+	ft_parsing(&pipex, argv, argc);
+
 	return (0);
 }
 
@@ -11,4 +14,24 @@ void	ft_get_init(t_grp *pipex)
 {
 	pipex->cmd1path = NULL;
 	pipex->cmd2path = NULL;
+}
+
+
+int	ft_parsing(t_grp *pipex, char **argv, int argc)
+{
+	ft_find_fd(pipex, argv, argc);
+	ft_find_path(pipex, argv, argc);
+}
+
+void	ft_find_fd(t_grp *pipex, char **argv, int argc)
+{
+	pipex->fd1 = open(argv[1], O_RDONLY);
+	close(pipex->fd1);
+	pipex->fd2 = open(argv[argc - 1], O_RDONLY);
+	close(pipex->fd2);
+}
+
+void	ft_find_path(t_grp *pipex, char **argv, int argc)
+{
+	
 }
--- pipex.h
+++ pipex.h
@@ -3,12 +3,16 @@
 # define PIPEX_H
 
 # include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <unistd.h>
 
 typedef struct s_grp
 {
 	char	*cmd1path;
 	char	*cmd2path;
-	int		infilefd;
-	int		outfilefd;
+	int		fd1;
+	int		fd2;
 }	t_grp;
 #endif
